47be8ec5b6b28cacf8d1f41840c7f175a9c11c3d
Phuwanart Larpmark <phuwanart@outlook.com>
2024-06-12 14:31:53 +0700

    general improvement

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,7 +4,11 @@ class Ingredient < ApplicationRecord
   has_many :cocktail_ingredients, dependent: :destroy
   has_many :cocktails, through: :cocktail_ingredients
 
+  def self.ransortable_attributes(_auth_object = nil)
+    column_names
+  end
+
   def self.ransackable_attributes(_auth_object = nil)
-    %w[name]
+    ransortable_attributes + _ransackers.keys
   end
 end
